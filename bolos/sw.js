8244e216e9068e2383a2ee33a7d505bd02d83ce0
Renato Silva <dominguesrds@fab.mil.br>
2026-05-07 23:17:02 -0300

    fix: add self.skipWaiting() to Service Workers to ensure immediate updates

--- bolos/sw.js
+++ bolos/sw.js
@@ -8,6 +8,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
@@ -22,6 +23,7 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
